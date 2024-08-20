d6e1135fce5283da9225a19b660fe27b6e015103
MINT <mint.2978@gmail.com>
2024-08-20 22:24:02 +0700

    feat: main page and auth with kinde

---
+++ src/lib/utils.ts
@@ -0,0 +1,6 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs)); //merge all the classes and return them,including deleted duplicates
+}
